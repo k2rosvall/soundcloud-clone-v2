6ce4d60bfe846194c04cfb45e910816d8d819d80
Karla Rosas <25908139+k2rosvall@users.noreply.github.com>
2025-04-11 08:37:01 -0600

    chore: create like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_07_131425) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_11_142034) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,6 +40,15 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_07_131425) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.bigint "profile_id", null: false
+    t.bigint "track_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_likes_on_profile_id"
+    t.index ["track_id"], name: "index_likes_on_track_id"
   end
 
   create_table "profiles", force: :cascade do |t|
@@ -76,6 +85,8 @@ ActiveRecord::Schema[7.2].define(version: 2025_04_07_131425) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "likes", "profiles"
+  add_foreign_key "likes", "tracks"
   add_foreign_key "profiles", "users"
   add_foreign_key "tracks", "profiles"
 end
